843cab98abb542e2068cb002ba4f4c699ddf8577
Kristen Chellis and William Chu <william@>
2018-03-20 15:49:25 -0700

    fix click bug

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,9 @@ $(function(){
     $("p").hide();
     $(this).find("p").slideDown();
     $(this).toggleClass("card");
-    $(this).click(function(){
-      $(this).find("p").toggle();
-      $(this).toggleClass("card");
-    });
+    //$(this).click(function(){
+      //$(this).find("p").toggle();
+      //$(this).toggleClass("card");
+    //});
   });
 });
