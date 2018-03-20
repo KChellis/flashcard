cd3e753d3e480e3d9afe78c6e40e78bedfff19c3
Kristen Chellis and William Chu <kristen@>
2018-03-20 13:51:30 -0700

    add deselect functionality and styling for card class

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 $(function(){
   $("div").click(function(){
-    $(this).find("p").toggle();
+    $("div").removeClass();
+    $("p").hide();
+    $(this).find("p").show();
     $(this).toggleClass("card");
+    $(this).click(function(){
+      $(this).find("p").toggle();
+      $(this).toggleClass("card");
+    });
   });
 });
